4faa4d413cb47b6aaa42b2df5c7f034585a41cc7
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2017-09-22 09:03:56 -0700

    cleaning up a bit

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,8 @@
-// index.ios.js - place code in here for android!!!
-
-// Import a library to help create a component
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
-//import ReactNative from 'react-native';
 
-// Create a component
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
@@ -19,4 +14,3 @@ const App = () => {
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
-//ReactNative.AppRegistry.registerComponent('albums', () => App);
